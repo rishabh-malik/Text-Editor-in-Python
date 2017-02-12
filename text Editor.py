3b3198d66913c245c242584194193385d1948663
rishabh <rishabhmalik249@gmail>
2017-02-12 14:48:58 -0300

    added func to menu

--- text Editor.py
+++ text Editor.py
@@ -2,30 +2,35 @@ from tkinter import *
 def doNothing():
     print("Ok")
 root= Tk()
+root.title("Notepad By Rishabh")
 root.geometry("400x400")
 
-def hello():
-    print ("hello!")
+def save():
+    file= open("file","w")
+    file.write(T.get("1.0",END))
+    file.close()
 
+def new():
+    T.delete(0.0,20.0)
 menubar = Menu(root)
 
 # create a pulldown menu, and add it to the menu bar
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Open", command=hello)
-filemenu.add_command(label="Save", command=hello)
+filemenu.add_command(label="New", command=new)
+filemenu.add_command(label="Save", command=save)
 filemenu.add_separator()
 filemenu.add_command(label="Exit", command=root.quit)
 menubar.add_cascade(label="File", menu=filemenu)
 
 # create more pulldown menus
 editmenu = Menu(menubar, tearoff=0)
-editmenu.add_command(label="Cut", command=hello)
-editmenu.add_command(label="Copy", command=hello)
-editmenu.add_command(label="Paste", command=hello)
+editmenu.add_command(label="Cut", command=doNothing())
+editmenu.add_command(label="Copy", command=doNothing())
+editmenu.add_command(label="Paste", command=doNothing())
 menubar.add_cascade(label="Edit", menu=editmenu)
 
 helpmenu = Menu(menubar, tearoff=0)
-helpmenu.add_command(label="About", command=hello)
+helpmenu.add_command(label="About", command=doNothing())
 menubar.add_cascade(label="Help", menu=helpmenu)
 
 # display the menu
@@ -34,4 +39,7 @@ root.config(menu=menubar)
 T = Text(root, height=400, width=400)
 T.pack()
 T.insert(END,root)
+
+
+
 root.mainloop()
